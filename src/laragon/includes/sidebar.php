85b5000da90a5fdc37387f3a1f5eefa9ff5a2ae1
Bryan Alejandro <brayan186930@outlook.com>
2024-11-17 10:15:55 -0600

    BD Checkin 1

--- src/laragon/includes/sidebar.php
+++
@@ -1,9 +0,0 @@
-<aside class="sidebar">
-    <a href="index.php?page=home" title="Inicio">🏠</a>
-    <a href="index.php?page=vuelos" title="Vuelos Disponibles">✈️</a>
-    <a href="index.php?page=settings" title="Configuración">⚙️</a>
-    <a href="index.php?page=checkin" title="Check-In">✅</a>
-    <a href="index.php?page=reservations" title="Reservas">📖</a>
-    <a href="index.php?page=details" title="Detalles de Vuelo">📄</a>
-    <a href="index.php?page=account" title="Usuario">👤</a>
-</aside>
